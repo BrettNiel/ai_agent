7c356231c8466b0f6e43134a86bbefae82da2754
BrettNiel <bmniel13@gmail.com>
2025-08-18 16:11:43 -0400

    added user input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,10 +10,16 @@ def main():
     
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
+    if len(sys.argv) > 1:
+        response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=sys.argv[1:]
     )
+    else:
+        print("Invalid prompt detected!")
+        print("Exiting")
+        sys.exit(1)
+
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
