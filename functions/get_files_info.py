e975d460da0875ae94faa52a5f8732a302fe1608
BrettNiel <bmniel13@gmail.com>
2025-09-23 11:49:36 -0400

    functions replacement

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,14 +5,19 @@ def get_files_info(working_directory, directory="."):
     absolute_path = os.path.abspath(working_directory)
     target_directory = os.path.abspath(os.path.join(working_directory, directory))
 
-    if target_directory.startswith(absolute_path) == False:
+    if not target_directory == absolute_path and not target_directory.startswith(absolute_path + os.sep):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    if os.path.isdir(directory) == False:
+    if not os.path.isdir(target_directory):
         return f'Error: "{directory}" is not a directory'
     
-    for file in directory:
+    directory_list = []
+    
+    for file in os.listdir(target_directory):
         try:
-            return f'- {os.path.basename(file)}: {os.path.getsize(file)} bytes, is_dir={os.path.isdir(file)}'
+            new_dir = os.path.join(target_directory, file)
+            directory_list.append(f'- {file}: file_size={os.path.getsize(new_dir)} bytes, is_dir={os.path.isdir(new_dir)}')
         except Exception as error:
             return f'Error: {error}'
+    
+    return "\n".join(directory_list)
