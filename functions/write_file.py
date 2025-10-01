bbe64164772f1bb080cd56c951797cb9949906a5
BrettNiel <bmniel13@gmail.com>
2025-10-01 19:58:13 -0400

    paths working, makedirs needs code

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,10 +2,22 @@ import os
 
 def write_file(working_directory, file_path, content):
     
-    absolute_path = os.path.abspath(working_directory)
-    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    absolute_path = os.path.realpath(working_directory)
+    intended_path = os.path.join(absolute_path, file_path)
+    true_path = os.path.realpath(intended_path)
     
-    if not file_path.startswith(absolute_path):
+    if not true_path.startswith(absolute_path):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     
-    
+    if not os.path.exists(true_path):
+        try:
+            os.makedirs(true_path)
+        except Exception as error:
+            return f'Error: {error}'
+        
+    with open(file_path, "w") as file:
+        try:
+            file.write(content)
+        except Exception as error:
+            return f'Error: {error}'
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
