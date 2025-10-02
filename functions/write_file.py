24af625dcde40136ed73009ffd15d3726ca9a9a5
BrettNiel <bmniel13@gmail.com>
2025-10-02 16:15:20 -0400

    finished write_file, started run_python_file

--- functions/write_file.py
+++ functions/write_file.py
@@ -8,14 +8,13 @@ def write_file(working_directory, file_path, content):
     
     if not true_path.startswith(absolute_path):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-    
-    if not os.path.exists(true_path):
-        try:
-            os.makedirs(true_path)
-        except Exception as error:
-            return f'Error: {error}'
+
+    try:
+        os.makedirs(os.path.dirname(true_path), exist_ok=True)
+    except Exception as error:
+        return f'Error: {error}'
         
-    with open(file_path, "w") as file:
+    with open(true_path, "w") as file:
         try:
             file.write(content)
         except Exception as error:
