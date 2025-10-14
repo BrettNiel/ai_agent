1593798c9838f6663073479fad6295f74abcbe5a
BrettNiel <bmniel13@gmail.com>
2025-10-14 15:21:55 -0400

    finished run_python_file func

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -19,10 +19,12 @@ def run_python_file(working_directory, file_path, args=[]):
     try:
         edited_arguments = ['python', true_path] + args
         result = subprocess.run(edited_arguments, timeout=30, cwd=working_directory, capture_output=True, check=True)
-        if result.stdout == None:
+        decoded_stdout = result.stdout.decode('utf-8')
+        decoded_stderr = result.stderr.decode('utf-8')
+        if decoded_stdout.strip() == '' and decoded_stderr.strip() == '':
             return f'No output produced'
-        return f'STDOUT: {result.stdout}, \nSTDERR:{result.stderr}'
+        return f'STDOUT: {decoded_stdout}, \nSTDERR:{decoded_stderr}'
     except subprocess.CalledProcessError as error:
-        return f'Process exited with code : {error}'
+        return f'Process exited with code : {error.returncode}'
     except Exception as error:
         return f'Error: executing Python file: {error}'
